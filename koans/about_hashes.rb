b6e9b7cb464f44a265715d4d8688b81f0046b4d9
Jason Noble <github+jasonn@jasonnoble.org>
2014-03-09 23:38:44 -0600

    Update about_hashes.rb

--- koans/about_hashes.rb
+++ koans/about_hashes.rb
@@ -2,7 +2,7 @@ require File.expand_path(File.dirname(__FILE__) + '/edgecase')
 
 class AboutHashes < EdgeCase::Koan
   def test_creating_hashes
-    empty_hash = Hash.new
+    empty_hash = Array.new
     assert_equal __, empty_hash.class
     assert_equal({}, empty_hash)
     assert_equal __, empty_hash.size
