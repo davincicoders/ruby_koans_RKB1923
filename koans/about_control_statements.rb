7d33681b3daf67155bb0cab4cef120cefd455e62
Rodney Bautista <Rodney.K.Bautista@gmail.com>
2014-03-19 18:54:55 -0600

    Fixes test_if_Statements_with_no_else_with_false    condition_return_value and 3 more
    * I learned that a statement with no false
      condition will return nil
    * I learned the syntax for condition operators
    * I learned about statement modifiers
    * I learned about the unless statement
    
    [#65851420]

--- koans/about_control_statements.rb
+++ koans/about_control_statements.rb
@@ -42,19 +42,19 @@ class AboutControlStatements < EdgeCase::Koan
     value = if false
               :true_value
             end
-    assert_equal __, value
+    assert_equal nil, value
   end
 
   def test_condition_operators
-    assert_equal __, (true ? :true_value : :false_value)
-    assert_equal __, (false ? :true_value : :false_value)
+    assert_equal :true_value, (true ? :true_value : :false_value)
+    assert_equal :false_value, (false ? :true_value : :false_value)
   end
 
   def test_if_statement_modifiers
     result = :default_value
     result = :true_value if true
 
-    assert_equal __, result
+    assert_equal :true_value, result
   end
 
   def test_unless_statement
@@ -62,7 +62,7 @@ class AboutControlStatements < EdgeCase::Koan
     unless false    # same as saying 'if !false', which evaluates as 'if true'
       result = :false_value
     end
-    assert_equal __, result
+    assert_equal :false_value, result
   end
 
   def test_unless_statement_evaluate_true
